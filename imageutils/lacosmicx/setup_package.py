adc1ce644a1488befde66f6d7008de354b7e5c2e
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-11-10 12:17:53 +0100

    Small fix to make sure things pass on Travis

--- imageutils/lacosmicx/setup_package.py
+++ imageutils/lacosmicx/setup_package.py
@@ -3,6 +3,10 @@ import os
 from distutils.core import setup, Extension
 
 
+# TODO: figure out how to set this conditionally for compilers that support it
+USE_OPENMP = False
+
+
 LACOSMICX_ROOT = os.path.relpath(os.path.dirname(__file__))
 
 
@@ -20,10 +24,11 @@ def get_extensions():
                     include_dirs=include_dirs,
                     libraries=libraries,
                     language="c",
-                    extra_compile_args=['-g', '-O3', '-fopenmp',
+                    extra_compile_args=['-g', '-O3',
                                         '-funroll-loops', '-ffast-math'])
 
-    # TODO: figure out how to put these only when not using clang:
-    # ext.extra_link_args = ['-g', '-fopenmp']
+    if USE_OPENMP:
+        ext.extra_compile_args.append('-fopenmp')
+        ext.extra_link_args = ['-g', '-fopenmp']
 
     return [ext]
